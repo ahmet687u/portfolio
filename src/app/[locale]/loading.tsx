d5ab568f7cc8bacc65487558ce922a6cf81d8d96
ahmet687u <biyikliahmet72@gmail.com>
2024-05-25 23:21:04 +0300

    loading component created

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,11 +1,17 @@
-
-
 const Loading = () => {
   return (
     <div className="loading">
-      Loading
+      <div className="loading-circle">
+        {new Array(7).fill("").map((_, index) => (
+          <div
+            key={index}
+            className="loading-circle-item"
+            style={{ ["--idc" as any]: index }}
+          ></div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
